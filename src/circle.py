60413c4dda7bc609945ea640521091d8c2285ab8
iksanova <limma@yandex.ru>
2022-10-16 22:13:36 +0300

    Figure_4

--- src/circle.py
+++ src/circle.py
@@ -2,6 +2,7 @@ from .figure import Figure
 
 class Circle(Figure):
     def __init__(self, a):
+        pi = 3.14
         super(Circle, self).__init__('Circle')
-        self._set_p(a*4)
+        self._set_p(pi * a * a)
         self._set_s(a*a)
--- src/square.py
+++ src/square.py
@@ -3,9 +3,8 @@ from .figure import Figure
 
 class Square(Figure):
     def __init__(self, a):
-        pi = 3.14
         super(Square, self).__init__('Square')
-        self._set_p(pi * a * a)
+        self._set_p(a*4)
         self._set_s(a*a)
 
     
